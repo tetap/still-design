3ba7cf1dc4d25028b891b51feb99d3a47ddb1442
zyc <239559238@qq.com>
2022-10-28 18:29:46 +0800

    'test'

--- packages/components/alert/index.ts
+++
@@ -1,9 +0,0 @@
-import { withInstall } from '@still-design/utils'
-
-import Alert from './src/alert.vue'
-
-export const STAlert = withInstall(Alert)
-
-export default STAlert
-
-export * from './src/alert'
--- packages/components/alert/src/alert.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="">alert</div>
-</template>
-
-<script lang="ts" setup></script>
-
-<style lang="scss" scoped></style>
